9bc7cf2e06f4de4a535bee3ef55f017c9d84671b
Charles Newey <charlie@assemblyco.de>
2014-12-11 22:29:50 +0000

    Test logging messages onto page

--- js/fb.js
+++ js/fb.js
@@ -1,14 +1,14 @@
 var app_options = {
-    "scope": 'user_groups'
+    "scope": "user_groups"
 }
 
-var acs = '/259914077434319/feed';
+var acs = "/259914077434319/feed";
 
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : '1517733728513665',
+        appId      : "1517733728513665",
         xfbml      : false,
-        version    : 'v2.1'
+        version    : "v2.1"
     });
 };
 
@@ -18,7 +18,7 @@ window.fbAsyncInit = function() {
     js = d.createElement(s); js.id = id;
     js.src = "http://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+ }(document, "script", "facebook-jssdk"));
 
 window.onload = function () {
     var loggedIn = false;
@@ -30,10 +30,11 @@ window.onload = function () {
             });
 
             // Loop through group feed
+            var post_elem = document.getElementById("posts");
             FB.api(acs, function (response) {
-                console.log(response);
-                for (var i = 0; i < response.length; i += 1) {
-                    console.log(JSON.stringify(response[i], null, "    "));
+                var posts = response.data;
+                for (var i = 0; i < posts.length; i += 1) {
+                    post_elem.appendChild("<div>" + posts[i].message + "</div>");
                 }
             });
         }
